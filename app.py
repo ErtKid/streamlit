1675078b6b5b9f99f5ed4431eae01f7c88e7f174
Phiboy <113423631+Phiboy@users.noreply.github.com>
2022-10-25 16:32:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib as plt
+import pyplot
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
